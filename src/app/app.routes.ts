d570c361fd4f66dc27906765f7fa837ff1be9863
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-22 13:54:51 -0600

    estructura de carpetas y enrutamiento a la seccion panel usando rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PanelComponent } from './main/panel/panel.component';
+import { HomeComponent } from './main/panel/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "panel", pathMatch: "full" },
+
+    //Panel principal
+  {
+    path: "panel",
+    component: PanelComponent,
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" }, 
+      { path: "home", component: HomeComponent }, 
+    ],
+  },
+
+    {path: "**", component: PanelComponent},
+
+];
